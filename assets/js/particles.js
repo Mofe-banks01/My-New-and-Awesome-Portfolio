818807c8e27da29b36f37eed6eecb9662a56d626
Mofe-banks01 <bankoleadedotun16@gmail.com>
2024-01-08 14:33:32 +0100

    Implementation Of Black and White design,Addition of Tektur Fonts

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -28,13 +28,13 @@ var pJS = function (tag_id, params) {
         },
       },
       color: {
-        value: "#fff",
+        value: "[#000]",
       },
       shape: {
         type: "circle",
         stroke: {
           width: 0,
-          color: "#ff0000",
+          color: "#000",
         },
         polygon: {
           nb_sides: 5,
